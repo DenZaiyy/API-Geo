811ffac3cd5c4fd5119e7a4779555f72661ef75a
DenZaiyy <denzaiyy@outlook.fr>
2025-01-27 10:43:24 +0100

    add: comment and readme with preview of code

---
+++ README.md
@@ -0,0 +1,22 @@
+# API Geo
+
+Cette exercice à pour but d'apprendre comment utiliser une API avec les bons endpoints pour récupérer les informations souhaités.
+
+## Exercice
+
+- Trouver le endpoint pour récupérer les villes selon le code postal
+- Crée un input pour saisir le code postal
+- Récupérer les informations d'une commune par son code postal
+- Crée un menu select pour choisir une commune
+- Afficher ces informations sur la carte
+
+## Ressources
+
+- [API Geo](https://geo.api.gouv.fr/)
+- [Leaflet](https://leafletjs.com/)
+- [TailwindCSS](https://tailwindcss.com/)
+
+## Aperçu du code
+
+- Partie du code qui permet de faire le call API selon le code postal saisie par l'utilisateur:
+![Image event input code postal](./src/inputCP.png)
--- js/script.js
+++ js/script.js
@@ -1,35 +1,59 @@
+// Instanciation des variables pour récuperer les éléments du DOM
 const inputCP = document.getElementById("codepostal");
 const selectVille = document.getElementById("ville");
-const map = L.map("map");
+
+// Instanciation de la carte avec les coordonnées de l'adresse d'Elan Formation par défaut
+const elan = [47.7272966, 7.2939856];
+let map = L.map("map").setView(elan, 15);
+let marker = L.marker(elan).addTo(map);
+var popup = L.popup()
+    .setLatLng([47.7282966, 7.2939856])
+    .setContent(
+        "<p>Welcome to Elan Formation<br />from Brunstatt-Didenheim</p>"
+    )
+    .openOn(map);
+// Instancier un tableau pour stocker les coordonnées des villes
 let coordinates = [];
 
+// Ajout de la couche OpenStreetMap
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 19,
     attribution:
         '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
 
+// Ecouter l'événement input sur l'input code postal pour pouvoir récupérer sa valeur et faire le call API
 inputCP.addEventListener("input", () => {
+    // Récupérer la valeur de l'input code postal
     let value = inputCP.value;
+    // Définir le contenu de la select a null
     selectVille.innerHTML = null;
 
+    // Faire le call API pour récupérer les villes correspondant au code postal saisi par l'utilisateur
     fetch(
         `https://geo.api.gouv.fr/communes?codePostal=${value}&fields=region,nom,code,codesPostaux,codeRegion,centre&format=json&geometry=centre`
     )
+        // Récupérer la réponse de l'API en format JSON
         .then((response) => response.json())
+        // Traiter les données récupérées
         .then((data) => {
-            console.log(data);
+            // Parcourir les données récupérées
             data.forEach((ville) => {
+                // Créer un élément option pour chaque ville
                 let option = document.createElement("option");
+                // Définir la valeur et le texte de l'option
                 option.value = `${ville.code}`;
                 option.innerHTML = `${ville.nom}`;
+                // Ajouter l'option à la select
                 selectVille.appendChild(option);
+                // Ajouter un marqueur pour chaque ville sur la carte
                 L.marker([
                     ville.centre.coordinates[1],
                     ville.centre.coordinates[0],
                 ])
                     .addTo(map)
                     .bindPopup(ville.nom);
+                // Ajouter les coordonnées de chaque ville dans le tableau coordinates
                 coordinates.push({
                     code: ville.code,
                     name: ville.nom,
@@ -40,11 +64,16 @@ inputCP.addEventListener("input", () => {
         });
 });
 
+// Ecouter l'événement change sur la select ville pour centrer la carte sur la ville sélectionnée
 selectVille.addEventListener("change", () => {
+    // Récupérer la valeur de la select ville
     let value = selectVille.value;
+    // Parcourir le tableau coordinates pour trouver les coordonnées de la ville sélectionnée
     coordinates.forEach((ville) => {
+        // Si le code de la ville correspond à la valeur de la select ville
         if (ville.code === value) {
-            map.setView([ville.lat, ville.lng], 14); // Inversé (latitude, longitude)
+            // Centrer la carte sur la ville sélectionnée
+            map.setView([ville.lat, ville.lng], 14);
         }
     });
 });
